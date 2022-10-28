94ca1faaf583b8d977ea2092d47bde3ec473e164
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-10-28 17:45:59 +0100

    feat: Implement GET / endpoint

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res, next) => {
+  return res.redirect("/profile");
+});
+
 app.get("/profile", (req, res, next) => {
   return res.status(200).json({
     slackUsername: "Omobolaji Sonde",
